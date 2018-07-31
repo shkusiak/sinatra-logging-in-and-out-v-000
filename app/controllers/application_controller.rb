1a9cd8f4f7eaba7e88ffef40efbe4f022fc4ed3e
Shea Kusiak <shkusiak@gmail.com>
2018-07-31 02:20:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(:username => params[:username])
     session[:user_id] = @user.id
-
+    redirect to '/account'
   end
 
   get '/account' do
